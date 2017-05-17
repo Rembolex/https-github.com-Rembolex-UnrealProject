9849498c5b921470295cfc80ca05e7e2f4b9f4cb
Alexander knol <alexander_1998@live.nl>
2017-05-17 21:09:52 +0200

    Inital Commit

---
+++ UnrealProject/Source/UnrealProject/UnrealProjectGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UnrealProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPROJECT_API AUnrealProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
